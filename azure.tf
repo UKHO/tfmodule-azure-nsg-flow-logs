77e617738bb14f194cf4f5408923e20ad3589f6d
Shedman <52528924+ShaunBresnahan@users.noreply.github.com>
2021-07-29 16:28:36 +0100

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -1,11 +1,7 @@
 provider "azurerm" {
-  features {}
   alias           = "nsgflow" 
-  subscription_id = var.subscription_id
 }
 
 provider "azurerm" {
-  features {}
-  alias           = "hub"
-  subscription_id = var.hub_subscription_id
+  alias           = "hub"  
 }
