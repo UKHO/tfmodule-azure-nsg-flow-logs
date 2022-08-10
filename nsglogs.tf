55c660e4ee3b84d1f591d9575da2f131011457e7
Shaun Bresnahan <ashaun@ukho.gov.uk>
2022-08-10 15:58:40 +0100

    update

--- nsglogs.tf
+++ nsglogs.tf
@@ -38,6 +38,7 @@ data "azurerm_log_analytics_workspace" "main" {
 }
 
 resource "azurerm_network_watcher_flow_log" "main" {
+  count = "${var.ignore_changes}"
   network_watcher_name = data.azurerm_network_watcher.main.name
   resource_group_name  = data.azurerm_resource_group.watcherrg.name
   name                 = "${var.spokensg}-flowlog"   
@@ -49,7 +50,8 @@ resource "azurerm_network_watcher_flow_log" "main" {
   enabled                   = true
   lifecycle { 
     ignore_changes = [
-      tags
+      tags,
+      location
    ] 
  }
 
@@ -66,4 +68,3 @@ resource "azurerm_network_watcher_flow_log" "main" {
     interval_in_minutes   = 60
   }
 }
-
